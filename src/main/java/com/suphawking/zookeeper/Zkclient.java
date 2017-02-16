a5bb1c67c29e30f32312ddfb26e9dd91ee7df782
真人 <weizhen.wang@duitang.com>
2017-02-16 19:20:58 +0800

    update

--- src/main/java/com/suphawking/zookeeper/Zkclient.java
+++ src/main/java/com/suphawking/zookeeper/Zkclient.java
@@ -25,11 +25,13 @@ public class Zkclient {
     Watcher watcher = null;
     ZooKeeper zk = new ZooKeeper(connectString, sessionTimeout, watcher);
     String a = new String(zk.getChildren("/rpc_groups/default_saturn", false).toString());
-    byte[] data = "".getBytes();
+    byte[] data = "abcd".getBytes();
+    zk.delete("/wwz/wwz",-1);
+    zk.delete("/wwz",-1);
     zk.create("/wwz",data,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
-    zk.create("/wwz/wwz", data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-
-
+    System.out.println(zk.create("/wwz/wwz", data, ZooDefs.Ids.OPEN_ACL_UNSAFE,
+        CreateMode.PERSISTENT));
+    System.out.println(zk.exists("/wwasdz/wwz",false));
 
 
   }
